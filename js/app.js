b06a351af97ee27f88963df9d5f7059774be35fd
jbm97 <macadamjake@gmail.com>
2024-04-02 23:09:17 -0400

    feat: complete header using javascript

--- js/app.js
+++ js/app.js
@@ -6,3 +6,38 @@
 
 /*==================== scroll reveal ====================*/
 /*==================== typed js ====================*/
+
+// Header
+const body = document.body;
+const header = document.createElement("header");
+header.className = "header";
+body.append(header);
+
+// menu icon
+const menuIcon = document.createElement("i");
+menuIcon.className = "bx bx-menu";
+menuIcon.id = "menu-icon";
+header.append(menuIcon);
+
+// Logo/title
+const logo = document.createElement("a");
+logo.href = "#";
+logo.className = "logo";
+logo.textContent = "Porfolio";
+header.append(logo);
+
+// Navbar
+const nav = document.createElement("nav");
+nav.className = "navbar";
+
+const links = ["Home", "About", "Services", "Portfolio", "Contact"];
+
+for (let i = 0; i < links.length; i++) {
+    const link = document.createElement("a");
+    link.href = "#";
+    link.textContent = links[i];
+    nav.append(link);
+}
+
+nav.children[0].className = "active"; // set home active
+header.append(nav);
