5140948c3690623740dafc02c81ca686313c33d9
jbm97 <macadamjake@gmail.com>
2024-04-06 15:49:59 -0400

    feat: make personal portfolio with html, css, js

--- js/app.js
+++ js/app.js
@@ -8,18 +8,59 @@ menuIcon.onclick = () => {
 };
 
 /*==================== scroll sections active link ====================*/
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
-/*==================== sticky navbar ====================*/
-let header = document.querySelector("header");
+window.onscroll = () => {
+    sections.forEach((sec) => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
 
-header.classList.toggle("sticky", window.scrollY > 100);
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach((links) => {
+                links.classList.remove("active");
+                document.querySelector("header nav a[href*=" + id + "]");
+            });
+        }
+    });
 
-/*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+    /*==================== sticky navbar ====================*/
+    let header = document.querySelector("header");
+
+    header.classList.toggle("sticky", window.scrollY > 100);
+
+    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+    menuIcon.classList.remove("bx-x");
+    navbar.classList.remove("active");
+};
 
 /*==================== scroll reveal ====================*/
-/*==================== typed js ====================*/
+ScrollReveal({
+    reset: true,
+    distance: "80px",
+    duration: 2000,
+    delay: 200,
+});
 
-// Dom Manip
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(".home-img, .services-container, .portfolio-box, .contact form", {
+    origin: "bottom",
+}),
+    ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content ", { origin: "right" });
+
+/*==================== typed js ====================*/
+const typed = new Typed(".multiple-text", {
+    strings: ["Full Stack Developer", "Gamer", "Professional Blackjack Player"],
+    typeSpeed: 100,
+    backSpeed: 70,
+    backDelay: 1500,
+    loop: true,
+});
+
+/*==================== DOM MANIP ====================*/
 // // Header
 // const body = document.body;
 // const header = document.createElement("header");
